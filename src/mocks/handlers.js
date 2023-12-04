f0d3b8dbf8798da878b9a48024210e067b52001b
Matthew Lean <matt@mattlean.com>
2023-12-03 22:30:25 -0800

    Add console.log in MSW handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,6 +10,7 @@ export const handlers = [
     // It accepts a bunch of information about the
     // intercepted request, and decides how to handle it.
     ({ request, params, cookies }) => {
+      console.log('Handler for /pets was reached.')
       return HttpResponse.json(['Tom', 'Jerry', 'Spike'])
     }
   )
